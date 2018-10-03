417222b1f04a54681dfc32798ca10cf4ecd01b32
Jack <jack.lafond@icloud.com>
2018-10-03 18:20:23 -0400

    Initial commit of unshorten command

---
+++ src/discord/commands/utilities/unshorten.ts
@@ -0,0 +1,39 @@
+import { Command, AccessLevel, Guards, Constants } from "dd-botkit";
+import { RichEmbed } from "discord.js";
+impoort { checkSpoopy } from "spoopylink";
+export const UnshortenCommand: Command = {
+    opts: {
+        name: "unshorten",
+        access: AccessLevel.EVERYONE,
+        category: "Utilities",
+        guards: [
+            Guards.Argumented(
+                "unshorten",
+                "Unshorten a link, as well as find out if the link is safe or not.",
+                [
+                    {
+                        name: "link",
+                        type: "string",
+                        required: true
+                    }
+                ]
+            )
+        ]
+    },
+    handler: async (message, next) => {
+        const [link] = msg.args;
+
+        if(!link) {
+          throw new CommandError({
+              message: "Please specify a link."
+          });
+        }
+
+        const spoop = await checkSpoopy(link)
+        const embed = new RichEmbed()
+        .setTitle(`${spoop.safe = false ? "Unsafe link/redirect detected" : "Safe link detected"}`)
+        .setColor(`${spoop.safe = false ? 0xFF0000 : 0x00ee00}`)
+
+        await message.channel.send(embed);
+    }
+};
