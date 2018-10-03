29330dba2bf9ea8bcf75d5e55e40c20b33cf0d91
Jack <jvck@protonmail.com>
2018-10-03 09:54:41 -0400

    Feature - nickname command (#5)
    * add nickname command
    
    * add say command
    
    * added msg.sucess()
    
    * Fixed args
    
    * Update nickname.ts

---
+++ src/discord/commands/moderation/nickname.ts
@@ -0,0 +1,41 @@
+import { Command, AccessLevel, CommandError, Logger, Guards } from "dd-botkit";
+
+export const NicknameCommand: Command = {
+    opts: {
+        name: "nickname",
+        access: AccessLevel.MODERATOR,
+        category: "Moderation",
+        guards: [
+            Guards.Argumented(
+                "nickname",
+                "Force changes the nickname of a user",
+                [
+                    {
+                        name: "user",
+                        type: "user",
+                        required: true
+                    },
+                    {
+                        name: "nickname",
+                        type: "string",
+                        required: true
+                    }
+                ]
+            )
+        ]
+    },
+    handler: async (msg, next) => {
+        const [user, nickname] = msg.args;
+        const member = msg.guild.members.get((user as User).id);
+
+
+        if (!member) {
+            throw new CommandError({
+                message: "Please specify a member of this guild."
+            });
+        }
+
+        await member.setNickname(nickname);
+        await msg.success();
+    }
+};
