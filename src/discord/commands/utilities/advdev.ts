bafefa3ed833e555e02469d9f534b8a6ff78865e
Oliver Boudet <oboudet1@me.com>
2018-10-06 20:44:34 +0200

    Advanced Dev command (#8)
    * add adv dev command
    
    * permission changes and error handling
    
    * change rejection/accept letter to some actual stuff
    
    * change rejection/accept letter to some actual stuff
    
    * guild check, as the command is only needed in one specific server
    
    * prettier-ignore some lines because ugly

---
+++ src/discord/commands/utilities/advdev.ts
@@ -0,0 +1,55 @@
+import { Command, AccessLevel, CommandError, Logger, Guards } from "dd-botkit";
+import { User } from "discord.js";
+export const AdvDevCommand: Command = {
+    opts: {
+        name: "advdev",
+        access: AccessLevel.ADMIN,
+        category: "Utilities",
+        guards: [
+            Guards.Argumented(
+                "advdev",
+                "Decline or accept a user to advanced developer.",
+                [
+                    {
+                        name: "Decline or accept",
+                        type: "boolean",
+                        required: true
+                    },
+                    {
+                        name: "user",
+                        type: "user",
+                        required: true,
+                        unlimited: true
+                    }
+                ]
+            )
+        ]
+    },
+    handler: async (msg, next) => {
+        if (
+            msg.guild.id == "349243932447604736" ||
+            msg.guild.id == "493378389802090498"
+        ) {
+            return await msg.fail();
+        }
+        // decision == false - decline
+        // decision == true - accept
+        const [decision, _user]: any | User = msg.args;
+        const user: User = _user;
+        const member = msg.guild.members.get(user.id);
+        if (!member) return;
+        if (decision) {
+            // Accept user.
+            //prettier-ignore
+            await user.send(`Hi ${user.username}}\nThank you for applying to the Advanced Developer role; we appreciate your work and enthusiasm. We are incredibly happy to inform you that your application has been approved 🎉. As you may know, this gives you access to #developer-backroom.\nThis channel is locked to the public, and nothing is to be leaked, without a developer's or and administrators permission. If you are caught sharing secret information, we will make the necessary decisions about your punishment.`);
+            await member.addRole("Advanced Developers");
+            return msg.success();
+        }
+        // Reject user.
+        //prettier-ignore
+        await user.send(`Hi ${user.username}\nThank you for applying for the Advanced Developer role; we appreciate your work and enthusiasm. We regret to inform you that your application for Advanced Developer was not accepted at this time, as unfortunately, we are not able to accept every application. We loved reading over your application, and encourage you to re-apply when you feel that you meet the criteria; you're more than welcome to do this in a months time. We look forward to see you in #development, and you'll still retain your Developer role. 
+
+        Best regards, 
+        the /r/jailbreak discord staff`);
+    }
+};
