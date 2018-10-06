88055ef50e39badc3b1bf69a101466b50c1f784f
Jack <jack.lafond@icloud.com>
2018-10-06 13:07:46 -0400

    rewrite, use url-unshort now

--- src/discord/commands/utilities/unshorten.ts
+++ src/discord/commands/utilities/unshorten.ts
@@ -1,6 +1,6 @@
 import { Command, AccessLevel, Guards, Constants } from "dd-botkit";
 import { RichEmbed } from "discord.js";
-import { checkSpoopy } from "spoopylink";
+import { uu } from "unshorten-url";
 export const UnshortenCommand: Command = {
     opts: {
         name: "unshorten",
@@ -9,7 +9,7 @@ export const UnshortenCommand: Command = {
         guards: [
             Guards.Argumented(
                 "unshorten",
-                "Unshorten a link, as well as find out if the link is safe or not.",
+                "Unshorten a link.",
                 [
                     {
                         name: "link",
@@ -21,28 +21,16 @@ export const UnshortenCommand: Command = {
         ]
     },
     handler: async (message, next) => {
-        const [link] = msg.args;
+        const [link] = message.args;
 
         if(!link) {
           throw new CommandError({
               message: "Please specify a link."
           });
         }
-
-        const spoop = await checkSpoopy(link)
-        const embed = new RichEmbed()
-        .setTitle(`${spoop.safe === false ? "Unsafe link detected" : "Safe link detected"}`);
-        .setColor(`${spoop.safe === false ? 0xFF0000 : 0x00ee00}`);
-        .addField(
-          "URL + Redirect Safety",
-          `${spoop.urlSafe = false ? 'This URL is not safe.' : 'This URL is deemed safe.'}\n${spoop.safe = false ? 'This URL and its redirects are not safe.' : 'This URL and its redirects are deemed safe.'}`,
-          true
-        );
-        .addField(
-          "Redirects",
-          `${spoop.redirects[0] ? spoop.redirects[0] : 'This URL has no redirects.'}`,
-          true
-        );
+        var url = await uu.expand(link)
+        const embed = new RichEmbed();
+        embed.setDescription(`${url}`)
         await message.channel.send(embed);
     }
 };
