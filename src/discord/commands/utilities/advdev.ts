b438ec89f6db3088c2ab57334ed7d293e821fdc9
Oliver Boudet <oboudet1@me.com>
2018-10-11 09:48:38 +0200

    fix lint errors

--- src/discord/commands/utilities/advdev.ts
+++ src/discord/commands/utilities/advdev.ts
@@ -1,5 +1,6 @@
 import { Command, AccessLevel, CommandError, Logger, Guards } from "dd-botkit";
-import { User } from "discord.js";
+import { User, GuildMember } from "discord.js";
+import { oneLine } from "common-tags";
 export const KickCommand: Command = {
     opts: {
         name: "advdev",
@@ -26,16 +27,34 @@ export const KickCommand: Command = {
         // decision == true - accept
         const [decision, _user]: any | User = message.args;
         const user: User = _user;
-        const member = message.guild.members.get(user.id);
-        if (!member) return;
+        const member: GuildMember | undefined = message.guild.members.get(
+            user.id
+        );
+        if (!member) {
+            return;
+        }
         if (decision) {
-            // Accept user.
-            await user.send(`Hi ${user.username}}\nThank you for applying to the Advanced Developer role; we appreciate your work and enthusiasm. We are incredibly happy to inform you that your application has been approved 🎉. As you may know, this gives you access to #developer-backroom.\nThis channel is locked to the public, and nothing is to be leaked, without a developer's or and administrators permission. If you are caught sharing secret information, we will make the necessary decisions about your punishment.`);
+            // accept user
+            await user.send(
+                oneLine`Hi ${
+                    user.username
+                }}\nThank you for applying to the Advanced Developer role; we appreciate your work and enthusiasm.
+                We are incredibly happy to inform you that your application has been approved 🎉.
+                As you may know, this gives you access to #developer-backroom.\n
+                This channel is locked to the public, and nothing is to be leaked, without a developer's or and administrators permission.
+                If you are caught sharing secret information, we will make the necessary decisions about your punishment.`
+            );
             await member.addRole("Advanced Developers");
             return message.success();
         }
-        // Reject user.
-        await user.send(`Hi ${user.username}\nThank you for applying for the Advanced Developer role; we appreciate your work and enthusiasm. We regret to inform you that your application for Advanced Developer was not accepted at this time, as unfortunately, we are not able to accept every application. We loved reading over your application, and encourage you to re-apply when you feel that you meet the criteria; you're more than welcome to do this in a months time. We look forward to see you in #development, and you'll still retain your Developer role. 
+        // reject user
+        // prettier-ignore
+        await user.send(oneLine`Hi ${user.username}\nThank you for applying for
+        the Advanced Developer role; we appreciate your work and enthusiasm.
+        We regret to inform you that your application for Advanced Developer was not accepted at this time, as unfortunately, 
+        we are not able to accept every application. 
+        We loved reading over your application, and encourage you to re-apply when you feel that you meet the criteria; 
+        you're more than welcome to do this in a months time. We look forward to see you in #development, and you'll still retain your Developer role. 
 
         Best regards, 
         the /r/jailbreak discord staff`);
